da6c94c0a7c4df6463f627b33851c792703cf5a2
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2019-10-06 14:57:03 +0200

    Add webpack config for developmnent

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,24 +1,48 @@
 const path = require('path');
-const { CleanWebpackPligin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
     entry: {
-        main: ''
+        main: './src/js/index.js'
     },
-
     mode: 'development',
-
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename:'',
+        filename:'js/[name].js',
         publicPath: '/'
      },
-
+     devServer: {
+        open: true,
+        contentBase: path.resolve(__dirname, '../', 'public'),
+        port: 3002
+     },
      module:{
-         rules:[]
+        rules:[
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
      },
 
-     plugins:[]
+     plugins:[
+         new CleanWebpackPlugin(),
+         new HtmlWebpackPlugin({
+             template: './index.html'
+         }),
+     ]
 };
