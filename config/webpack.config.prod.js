26fe168c2e1f5713fb2a023676dd3e6536f2f491
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2019-10-06 16:45:58 +0200

    Add webpack config for production

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,24 +1,72 @@
 const path = require('path');
-const { CleanWebpackPligin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-
     entry: {
-        main: ''
+        main: './src/js/index.js'
     },
-
-    mode: 'development',
-
+    mode: 'production',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename:'',
+        path: path.resolve(__dirname, '../', 'build'),
+        filename:'js/[name]-[contenthash:5].js',
         publicPath: '/'
      },
-
      module:{
-         rules:[]
+        rules:[
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name]-[contenthash:5].[ext]',
+                            outputPath: 'images',
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                quality: 70
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
      },
 
-     plugins:[]
+     plugins:[
+         new CleanWebpackPlugin(),
+         new HtmlWebpackPlugin({
+             template: 'index.html'
+         }),
+         new MiniCssExtractPlugin({
+             filename: '[name]-[contenthash:5].css'
+         }),
+         new CopyPlugin(
+            [
+                {
+                    from: 'public/images',
+                    to: 'images'
+                }
+            ]
+        )
+     ]
 };
