c9de62f10644d1978c196f1a0aede2308ecfff4d
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2019-10-13 20:16:29 +0200

    Add styles for header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 import '../sass/style.scss';
+
+
+// sidebar
+const sidebarBtn = document.querySelector('#menu-hamburger'),
+    sidebarBox = document.querySelector('#menu-content');
+
+
+sidebarBtn.addEventListener('click', function (event) {
+    sidebarBtn.classList.toggle('active');
+    sidebarBox.classList.toggle('active');
+});
+
+sidebarBox.addEventListener('click', function (event) {
+    sidebarBtn.classList.toggle('active');
+    sidebarBox.classList.toggle('active');
+});
