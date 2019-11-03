992e71dcc7480b2caf7fd329ea3547bec2432af0
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2019-11-03 17:12:04 +0100

    Add scrollTop and scrollReveal to page

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 import '../sass/style.scss';
+import $ from 'jquery';
 
+
+// scroll top page
+$('.arrow-top__img').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 600);
+});
 
 // sidebar
 const sidebarBtn = document.querySelector('#menu-hamburger'),
@@ -14,4 +22,10 @@ sidebarBtn.addEventListener('click', function (event) {
 sidebarBox.addEventListener('click', function (event) {
     sidebarBtn.classList.toggle('active');
     sidebarBox.classList.toggle('active');
+});
+
+// ScrollReveal
+ScrollReveal().reveal('span', {
+    delay: 200,
+    duration: 600
 });
