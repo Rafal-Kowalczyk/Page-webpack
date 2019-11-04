82eeb7bb5c4e1aecce337a6b1b64cda34abae75a
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2019-11-04 21:29:41 +0100

    Fix scss rules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,7 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
@@ -11,39 +13,25 @@ module.exports = {
     mode: 'production',
     output: {
         path: path.resolve(__dirname, '../', 'build'),
-        filename:'js/[name]-[contenthash:5].js',
-        publicPath: '/'
-     },
-     module:{
-        rules:[
-            {
+        filename: 'js/[name]-[contenthash:5].js'
+    },
+    module: {
+        rules: [{
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [require('autoprefixer')],
-                        }
-                    }
+                    'css-loader'
                 ]
             },
             {
                 test: /\.(sass|scss)$/,
                 use: [MiniCssExtractPlugin.loader,
                     'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [require('autoprefixer')],
-                        }
-                    },
-                    'sass-loader']
+                    'sass-loader'
+                ]
             },
             {
                 test: /\.(jpg|jpeg|png|gif|svg)$/,
-                use: [
-                    {
+                use: [{
                         loader: 'file-loader',
                         options: {
                             name: '[name]-[contenthash:5].[ext]',
@@ -62,27 +50,36 @@ module.exports = {
             },
             {
                 test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-     },
-
-     plugins:[
-         new CleanWebpackPlugin(),
-         new HtmlWebpackPlugin({
-             template: 'index.html'
-         }),
-         new MiniCssExtractPlugin({
-             filename: '[name]-[contenthash:5].css'
-         }),
-         new CopyPlugin(
-            [
-                {
-                    from: 'public/images',
-                    to: 'images'
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        ["@babel/preset-env", {
+                            useBuiltIns: 'usage',
+                            corejs: "2.0.0"
+                        }]
+                    ],
+                    plugins: [
+                        "@babel/plugin-proposal-class-properties"
+                    ]
                 }
-            ]
+            },
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name]-[contenthash:5].css'
+        }),
+        new CopyPlugin(
+            [{
+                from: 'public/images',
+                to: 'images'
+            }]
         )
-     ]
+    ]
 };
